1481368916ed9a4b162ee7e2a4f80a8124e221bc
tasmm-iscte <tasmm@iscte-iul.pt>
2021-02-26 17:13:20 +0000

    mkn

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 	
 	
-	public void main (String[] args) {
+	public static void main (String[] args) {
 		
 		System.out.println("sfognsjdgnjsdgn");
 		
@@ -10,9 +10,7 @@ public class Main {
 		
 		
 		
-		
-		
-		
+	
 	}
 	
 
